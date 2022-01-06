717aa27b117ab7d26d72cb7e9db4e1c854529713
Ionut Alexandru Muresan <alexandru.muresan@fortech.ro>
2022-01-06 16:03:11 +0200

    save

--- ninja-jobs/src/views/About.vue
+++ ninja-jobs/src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia ea,
+      assumenda expedita nemo dolore reiciendis exercitationem nesciunt neque id
+      ad, veritatis vero nihil ex aperiam error molestias est quibusdam commodi
+      sunt consequatur ipsa quo dolorum. Natus cupiditate perspiciatis earum
+      nesciunt tenetur, sint nulla, harum ipsum reprehenderit accusantium
+      aperiam reiciendis, beatae quaerat adipisci illum! Quae explicabo saepe
+      ipsa eum recusandae vel nihil, est quasi porro illo asperiores dolore,
+      cupiditate, laboriosam in. Repudiandae nisi nobis dolore ut quidem, qui
+      ipsa sit illum repellat eaque et! Ut id provident cum ducimus ad dolores
+      tempora sint. Voluptate distinctio ex eligendi itaque quaerat porro autem!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia ea,
+      assumenda expedita nemo dolore reiciendis exercitationem nesciunt neque id
+      ad, veritatis vero nihil ex aperiam error molestias est quibusdam commodi
+      sunt consequatur ipsa quo dolorum. Natus cupiditate perspiciatis earum
+      nesciunt tenetur, sint nulla, harum ipsum reprehenderit accusantium
+      aperiam reiciendis, beatae quaerat adipisci illum! Quae explicabo saepe
+      ipsa eum recusandae vel nihil, est quasi porro illo asperiores dolore,
+      cupiditate, laboriosam in. Repudiandae nisi nobis dolore ut quidem, qui
+      ipsa sit illum repellat eaque et! Ut id provident cum ducimus ad dolores
+      tempora sint. Voluptate distinctio ex eligendi itaque quaerat porro autem!
+    </p>
   </div>
 </template>
--- ninja-jobs/src/views/Home.vue
+++ ninja-jobs/src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda id fuga
+      minima, quas molestias aspernatur qui amet iure eligendi commodi deserunt
+      optio vitae libero sed saepe odit omnis! Quos temporibus officiis dolorum?
+      Amet corporis illo rerum ducimus. Ab asperiores illum incidunt, quisquam
+      eius nostrum ducimus laborum, dignissimos quidem iure inventore molestias
+      repudiandae, hic veritatis fugit esse. Sunt explicabo consectetur
+      praesentium minima temporibus ipsum voluptate accusantium. Quas, ullam
+      nisi eos dolores et quaerat accusamus corrupti, incidunt minus
+      perspiciatis ea saepe. Eligendi obcaecati omnis delectus nemo quam id
+      tenetur at recusandae, doloremque illum fuga nihil, perferendis sequi
+      quaerat! Consectetur itaque voluptatum illum!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda id fuga
+      minima, quas molestias aspernatur qui amet iure eligendi commodi deserunt
+      optio vitae libero sed saepe odit omnis! Quos temporibus officiis dolorum?
+      Amet corporis illo rerum ducimus. Ab asperiores illum incidunt, quisquam
+      eius nostrum ducimus laborum, dignissimos quidem iure inventore molestias
+      repudiandae, hic veritatis fugit esse. Sunt explicabo consectetur
+      praesentium minima temporibus ipsum voluptate accusantium. Quas, ullam
+      nisi eos dolores et quaerat accusamus corrupti, incidunt minus
+      perspiciatis ea saepe. Eligendi obcaecati omnis delectus nemo quam id
+      tenetur at recusandae, doloremque illum fuga nihil, perferendis sequi
+      quaerat! Consectetur itaque voluptatum illum!
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
