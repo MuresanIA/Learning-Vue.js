30c3339e7456cd2b9c82fe60fb8539a4536c4361
Ionut Alexandru Muresan <alexandru.muresan@fortech.ro>
2022-01-06 13:27:20 +0200

    save

--- reaction-timer/src/components/Block.vue
+++ reaction-timer/src/components/Block.vue
@@ -11,18 +11,10 @@ export default {
     };
   },
   mounted() {
-    console.log("component mounted");
     setTimeout(() => {
       this.showBlock = true;
-      console.log(this.delay);
     }, this.delay);
   },
-  updated() {
-      console.log('component updated');
-  },
-  unmounted() {
-      console.log('unmounted');
-  }
 };
 </script>
 
