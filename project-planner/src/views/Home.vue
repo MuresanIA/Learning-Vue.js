73b1d83f69dbb545cca1ae0c4599515ecacf9ecf
Ionut Alexandru Muresan <alexandru.muresan@fortech.ro>
2022-01-07 13:36:34 +0200

    save

--- project-planner/src/views/Home.vue
+++ project-planner/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="projects.length">
     <div v-for="project in projects" :key="project.id">
-      <SingleProject :project="project"/>
+      <SingleProject :project="project" @delete="handleDelete" />
     </div>
   </div>
   <div v-else>
@@ -14,11 +14,18 @@
 import SingleProject from "../components/SingleProject.vue";
 export default {
   name: "Home",
-  components: {SingleProject},
+  components: { SingleProject },
   data() {
     return {
       projects: [],
     };
+  },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter((project) => {
+        return project.id !== id;
+      });
+    },
   },
   mounted() {
     fetch("http://localhost:3000/projects")
