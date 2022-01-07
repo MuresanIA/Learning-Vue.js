2c6b6ea332a080cf67a4c6e8e6f2da1e0bb16bfe
Ionut Alexandru Muresan <alexandru.muresan@fortech.ro>
2022-01-07 09:43:20 +0200

    save

--- ninja-jobs/src/router/index.js
+++ ninja-jobs/src/router/index.js
@@ -1,30 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
-import About from '../views/About.vue';
-import NotFound from '../views/NotFound.vue';
-import Jobs from '../views/jobs/Jobs.vue';
-import JobDetails from '../views/jobs/JobDetails.vue';
 
 const routes = [
 	{
 		path: '/',
 		name: 'Home',
-		component: Home
+		component: () => import('../views/Home.vue')
 	},
 	{
 		path: '/about',
 		name: 'About',
-		component: About
+		component: () => import('../views/About.vue')
 	},
 	{
 		path: '/jobs',
 		name: 'Jobs',
-		component: Jobs
+		component: () => import('../views/Jobs.vue')
 	},
 	{
 		path: '/jobs/:id',
 		name: 'JobDetails',
-		component: JobDetails,
+		component: () => import('../views/JobDetails.vue'),
 		props: true
 	},
 	// redirect
@@ -36,7 +31,7 @@ const routes = [
 	{
 		path: '/:catchAll(.*)',
 		name: 'NotFound',
-		component: NotFound,
+		component: () => import('../views/NotFound.vue')
 	}
 ];
 
