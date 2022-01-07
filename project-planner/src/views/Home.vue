ea3b2ce6bd37db53e304ff18d8d4d8d1b74ff653
Ionut Alexandru Muresan <alexandru.muresan@fortech.ro>
2022-01-07 11:23:23 +0200

    save

--- project-planner/src/views/Home.vue
+++ project-planner/src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="home">
-   Home
+  <div class="home">Home</div>
+  <div v-if="projects.length">
+    <div v-for="project in projects" :key="project.id">
+      <p>{{ project.title }}</p>
+    </div>
+  </div>
+  <div v-else>
+    <h3>Loading projects ...</h3>
   </div>
 </template>
 
@@ -8,7 +14,18 @@
 // @ is an alias to /src
 
 export default {
-  name: 'Home',
-  components: {}
-}
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
