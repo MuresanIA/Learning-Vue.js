3eb5325b7c22ef8b79540ea4bd76ba03f359389e
Ionut Alexandru Muresan <alexandru.muresan@fortech.ro>
2022-01-10 15:39:26 +0200

    save

--- dojo-blog/src/views/Create.vue
+++ dojo-blog/src/views/Create.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="create">
-    <form>
+    <form @submit.prevent="handleSubmit">
       <label>Title:</label>
       <input v-model="title" type="text" required />
       <label>Content</label>
@@ -30,7 +30,19 @@ export default {
       tag.value = "";
     };
 
-    return { title, body, tag, handleKeydown, tags };
+    const handleSubmit = async () => {
+      const post = {
+        title: title.value,
+        body: body.value,
+        tags: tags.value,
+      };
+      await fetch("http://localhost:3000/posts", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(post),
+      });
+    };
+    return { title, body, tag, handleKeydown, tags, handleSubmit };
   },
 };
 </script>
@@ -52,44 +64,44 @@ textarea {
   border: 1px solid #eee;
 }
 textarea {
-    height: 160px;
+  height: 160px;
 }
 label {
-    display: inline-block;
-    margin-top: 30px;
-    position: relative;
-    font-size: 20px;
-    color: white;
-    margin-bottom: 10px;
+  display: inline-block;
+  margin-top: 30px;
+  position: relative;
+  font-size: 20px;
+  color: white;
+  margin-bottom: 10px;
 }
 label::before {
-    content: "";
-    display: block ;
-    width: 100%;
-    height: 100%;
-    background: #ff8800;
-    position: absolute;
-    z-index: -1;
-    padding-right: 40px;
-    left: -30px;
-    transform: rotateZ(-1.5deg);
+  content: "";
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: #ff8800;
+  position: absolute;
+  z-index: -1;
+  padding-right: 40px;
+  left: -30px;
+  transform: rotateZ(-1.5deg);
 }
 button {
-    display: block;
-    margin-top: 30px;
-    background: #ff8800;
-    color: white;
-    border: none;
-    padding: 8px 16px;
-    font-size: 18px;
+  display: block;
+  margin-top: 30px;
+  background: #ff8800;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  font-size: 18px;
 }
 .pill {
-    display: inline-block;
-    margin: 10px 10px 0 0;
-    color: #444;
-    background: #ddd;
-    padding: 8px;
-    border-radius: 20px;
-    font-size: 14px;
+  display: inline-block;
+  margin: 10px 10px 0 0;
+  color: #444;
+  background: #ddd;
+  padding: 8px;
+  border-radius: 20px;
+  font-size: 14px;
 }
 </style>
