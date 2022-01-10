c59571a3f1b4ae4a070220952a99317259dd03ed
Ionut Alexandru Muresan <alexandru.muresan@fortech.ro>
2022-01-10 10:10:15 +0200

    save

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -2,26 +2,34 @@
   <p>Home</p>
 
   <p>My name is : {{ name }}</p>
-  <p>My age is : {{ age }}</p>
+  <p ref="p">My age is : {{ age }}</p>
   <button @click="handleClick">click me</button>
 </template>
 
 <script>
+import { ref } from "@vue/reactivity";
 // @ is an alias to /src
 export default {
   name: "Home",
   components: {},
   setup() {
+    console.log(this);
+    let p = ref(null);
+    console.log(p, p.value);
+
     let name = "Mario";
     let age = 30;
 
     const handleClick = () => {
-      console.log("you clicked me");
+      console.log(p, p.value);
+      p.value.classList.add("test");
+      p.value.textContent = "hello, ninjas";
     };
     return {
       name,
       age,
-      handleClick
+      handleClick,
+      p,
     };
   },
 };
