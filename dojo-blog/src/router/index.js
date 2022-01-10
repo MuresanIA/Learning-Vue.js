11d6cf8c61adf33217653f5020dfffb91842f412
Ionut Alexandru Muresan <alexandru.muresan@fortech.ro>
2022-01-10 13:57:22 +0200

    save

--- dojo-blog/src/router/index.js
+++ dojo-blog/src/router/index.js
@@ -1,16 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import ('../views/Home.vue')
-  },
-]
+	{
+		path: '/',
+		name: 'Home',
+		component: () => import('../views/Home.vue')
+	},
+	{
+		path: '/posts/:id',
+		name: 'Details',
+		component: () => import('../views/Details.vue'),
+		props: true
+	}
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+	history: createWebHistory(process.env.BASE_URL),
+	routes
+});
 
-export default router
+export default router;
